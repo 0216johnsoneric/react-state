5b8a6b07e563972cc63799999421da394b486a24
Eric Johnson <ericjohnson0216@icloud.com>
2020-12-14 20:50:34 -0500

    Done.

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -3,10 +3,26 @@ import Container from "../../components/Container";
 import Col from "../../components/Col";
 import Row from "../../components/Row";
 
-const Signup = () => {
+//create state function and define useState for username and password
+function Signup() {
+  const [username, setUsername ] = useState("");
+  const [password, setPassword ] = useState("");
+
   const handleSubmit = e => {
-    e.preventDefault();
+    e.preventDefault()
   };
+
+  const handleUsernameChange =(e) => {
+    setUsername(e.target.value)
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value)
+  };
+// const Signup = () => {
+//   const handleSubmit = e => {
+//     e.preventDefault();
+//   };
 
   return (
     <div>
@@ -17,7 +33,15 @@ const Signup = () => {
         <Container className="mt-3 px-5">
           <Row className="form-group">
             <Col size="12">
-              <input className="form-control" type="text" placeholder="Username" name="username" />
+              <input 
+                className="form-control" 
+                type="text" 
+                placeholder="Username" 
+                name="username" 
+                value={username}
+                onChange={handleUsernameChange}
+                // onChange={(e)=> setUsername(e.target.value)}
+              />
             </Col>
           </Row>
           <Row className="form-group">
@@ -27,6 +51,9 @@ const Signup = () => {
                 type="password"
                 placeholder="Password"
                 name="password"
+                value={password}
+                onChange={handlePasswordChange}
+                // onChange={(e)=> setPassword(e.target.value)}
               />
             </Col>
           </Row>
@@ -35,12 +62,13 @@ const Signup = () => {
           </button>
         </Container>
         <Container className="mt-4">
-          <h3>Hello NAME_HERE!</h3>
-          <p>I probably shouldn't tell you this, but your password is PASSWORD_HERE}!</p>
+          <h3>Hello {username}!</h3>
+          <p>I probably shouldn't tell you this, but your password is {password}!</p>
         </Container>
       </form>
     </div>
   );
-};
+  };
+
 
 export default Signup;
